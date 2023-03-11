c0920766794f6a1b578a643a9f682a4e1801f2a5
Zhe Zhang <zhezhang@thoughtworks.com>
2023-03-11 20:11:01 +0100

    fix: remove unnecessay files and formatting

--- financialJanitor/src/Transaction.java
+++ financialJanitor/src/Transaction.java
@@ -1,15 +1,12 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Transaction {
     public static void main(String[] args) {
         System.out.println("This is a default account information:");// Print prompt
         double currentBalance = 234;
-        System.out.println("The account number is: 123456789." + "The balance is "+ currentBalance + " Euro."); // Print ID
+        System.out.println("The account number is: 123456789." + "The balance is " + currentBalance + " Euro."); // Print ID
         System.out.println("The following information is about transaction: "); // Print account balance
-        for (String i : args){
+        for (String i : args) {
             System.out.println("");
-            System.out.println("The amount of transaction is "+ i + " Euro. ");
+            System.out.println("The amount of transaction is " + i + " Euro. ");
             double transactionAmount = Double.parseDouble(i);
             currentBalance += transactionAmount;
             System.out.println("The latest account balance is " + currentBalance + " Euro.");
