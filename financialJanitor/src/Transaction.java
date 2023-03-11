22504670b2e7844e764cdb2d0fc30e52d476e0bf
Zhe Zhang <zhezhang@thoughtworks.com>
2023-03-11 21:36:39 +0100

    track balance with data file

--- financialJanitor/src/Transaction.java
+++ financialJanitor/src/Transaction.java
@@ -1,7 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+
 public class Transaction {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("This is a default account information:");// Print prompt
-        double currentBalance = 234;
+        final Path balanceFile = Path.of("balance.txt");
+        double currentBalance;
+        try {
+            final byte[] bytes;
+            bytes = Files.readAllBytes(balanceFile);
+            currentBalance = Double.parseDouble(new String(bytes));
+        } catch (NoSuchFileException e) {
+            Files.write(balanceFile, Double.toString(0).getBytes());
+            currentBalance = 0;
+        }
         System.out.println("The account number is: 123456789." + "The balance is " + currentBalance + " Euro."); // Print ID
         System.out.println("The following information is about transaction: "); // Print account balance
         for (String i : args) {
@@ -11,5 +25,6 @@ public class Transaction {
             currentBalance += transactionAmount;
             System.out.println("The latest account balance is " + currentBalance + " Euro.");
         }
+        Files.write(balanceFile, Double.toString(currentBalance).getBytes());
     }
 }
