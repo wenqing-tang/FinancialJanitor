2c29fc070e26f78c05da311caee3b277654f2434
wenqing-tang <tangwqlucky@gmail.com>
2023-03-12 01:29:43 +0100

    Add log, show commands and prompt

--- financialJanitor/src/Application.java
+++ financialJanitor/src/Application.java
@@ -5,9 +5,19 @@ public class Application {
     public static void main(String[] args) {
         final BalanceRepository balanceRepository = new BalanceRepository();
         final TransactionService transactionService = new TransactionService(balanceRepository);
+        if (args[0].equals("log")) {
+            Arrays.stream(args, 1, args.length)
+                    .map(Double::parseDouble)
+                    .forEach(transactionService::logTransaction);
+        } else if (args[0].equals("show")) {
+            System.out.println("The balance is " + balanceRepository.getBalance());
 
-        Arrays.stream(args)
-                .map(Double::parseDouble)
-                .forEach(transactionService::logTransaction);
+        } else {
+            System.out.println("""
+                    Usage: ./run.sh command
+                    commands:
+                    - log
+                    - show""");
+        }
     }
 }
