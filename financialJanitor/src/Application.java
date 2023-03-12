27e0c3b1f990889ebf7706f55945cefe852fd7e7
Zhe Zhang <zhezhang@thoughtworks.com>
2023-03-12 13:59:24 +0100

    refactoring: replace if-else with switch-case

--- financialJanitor/src/Application.java
+++ financialJanitor/src/Application.java
@@ -7,14 +7,15 @@ public class Application {
     public static void main(String[] args) {
         final BalanceRepository balanceRepository = new BalanceRepository();
         final TransactionService transactionService = new TransactionService(balanceRepository);
-        if (args[0].equals("log")) {
-            final List<Double> transactions = Arrays.stream(args, 1, args.length)
-                    .map(Double::parseDouble).collect(Collectors.toList());
-            transactionService.logTransactions(transactions);
-        } else if (args[0].equals("show")) {
-            transactionService.showBalance();
-        } else {
-            System.out.println("""
+        switch (args[0]) {
+            case "log" -> {
+                final List<Double> transactions = Arrays.stream(args, 1, args.length)
+                        .map(Double::parseDouble)
+                        .collect(Collectors.toList());
+                transactionService.logTransactions(transactions);
+            }
+            case "show" -> transactionService.showBalance();
+            default -> System.out.println("""
                     Usage: ./run.sh command
                     commands:
                     - log
